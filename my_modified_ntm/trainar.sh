e590cbba9b7742a1a131e0397dcd732349e2f624
hirame3881 <koki.tomei@gmail.com>
2023-01-06 10:25:06 +0900

    v1228 associative

--- my_modified_ntm/trainar.sh
+++ my_modified_ntm/trainar.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-command="python3 train_arps.py --task_type associative -task_json ntm/tasks/associative.json -saved_model saved_models/saved_model_associative.pt -batch_size 250 -num_iters 800 -summarize_freq 8 -lr 1e-3 --device cuda"
+command="python3 train_arps.py --task_type associative -task_json ntm/tasks/associative.json -saved_model saved_models/saved_model_associative.pt -batch_size 250 -num_iters 12000 -summarize_freq 120 -lr 1e-3 --device cuda"
 
 for runid in `seq 1 5`
 do
